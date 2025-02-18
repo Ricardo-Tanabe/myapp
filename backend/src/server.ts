bf090c5b6f4ad52444c3199a0048859a52e92ea4
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-18 15:42:57 -0300

    Adição do HOST 127.0.0.1 para testes locais

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
 import app from "./app";
 
 // FUTURE: Add database connection before starting the server.
-const PORT = process.env.PORT || 3001;
+const PORT = Number(process.env.PORT) || 3001;
 const HOST = "127.0.0.1"
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
     console.log(`Server running on http://${HOST}:${PORT}`);
 });
