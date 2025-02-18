3210214f809dbd11666dad32fdb6d33d613616ae
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-18 16:30:14 -0300

    Descrição de cada middleware utilizado no app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,15 +6,15 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 // FUTURE: Add database connection before starting the server.
-app.use(cors({
+app.use(cors({ // Allows the frontend to access the backend
     origin: "http://localhost:3000",
     credentials: true,
 }));
 
-app.use(cookieParser());
+app.use(cookieParser()); // Allows the backend to read cookies in req.cookies
 
-app.use(express.json());
+app.use(express.json()); // Allows receiving JSON in req.body
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes); // Authorization routes
 
 export default app;
