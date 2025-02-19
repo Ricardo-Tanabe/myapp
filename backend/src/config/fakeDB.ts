c49c2ee2d1a793066a1705808c3a9e9012ac0184
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-19 10:28:54 -0300

    fakeDB configurado para ter seu password criptografado

--- backend/src/config/fakeDB.ts
+++ backend/src/config/fakeDB.ts
@@ -1,7 +1,13 @@
 // TEMPORARY: Dummy database. Replace it with a real one in the future.
+import { hashPassword } from "../utils/hash"
+
+async function initializeDB() {
+    const hashedPassword = await hashPassword("password123");
+    fakeDB.users.push({ email: "test@email.com", password: hashedPassword })
+}
 
 export const fakeDB = {
-    users: [
-        { email: "test@email.com", password: "password123" }
-    ]
+    users: [] as { email: string, password: string }[],
 };
+
+initializeDB().then(() => console.log("fakeDB initialized"));
