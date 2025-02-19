7de119003c3928b1dc256f0c4f1d82c856ae5343
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-19 11:54:12 -0300

    Pequena modificação

--- backend/src/config/fakeDB.ts
+++ backend/src/config/fakeDB.ts
@@ -1,13 +1,13 @@
 // TEMPORARY: Dummy database. Replace it with a real one in the future.
 import { hashPassword } from "../utils/hash"
 
+export const fakeDB = {
+    users: [] as { email: string, password: string }[],
+};
+
 async function initializeDB() {
     const hashedPassword = await hashPassword("password123");
     fakeDB.users.push({ email: "test@email.com", password: hashedPassword })
 }
-
-export const fakeDB = {
-    users: [] as { email: string, password: string }[],
-};
 
 initializeDB().then(() => console.log("fakeDB initialized"));
