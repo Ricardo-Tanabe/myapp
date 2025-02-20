a1e8f25138f6ea9c3c243b23c24e985d38393c7f
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-20 16:56:14 -0300

    Mudança do HOST para localhost

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import app from "./app";
 
 // FUTURE: Add database connection before starting the server.
 const PORT = Number(process.env.PORT) || 3001;
-const HOST = "127.0.0.1"
+const HOST = "localhost"
 
 app.listen(PORT, HOST, () => {
     console.log(`Server running on http://${HOST}:${PORT}`);
