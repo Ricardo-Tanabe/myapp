d8ce0e092238e65858c7099db46d857b89b775fe
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-21 14:14:33 -0300

    Atualização da estrutura para padronização do middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,16 +3,21 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
     const API_URL = "http://localhost:5000/api/auth"
 
-    const res = await fetch(`${API_URL}/me`, {
-        headers: {Cookie: req.headers.get("cookie") || ""},
-        credentials: "include",
-    })
-
-    if(res.status !== 200 && req.nextUrl.pathname.startsWith("/dashboard")) {
+    try {
+        const res = await fetch(`${API_URL}/me`, {
+            headers: {Cookie: req.headers.get("cookie") || ""},
+            credentials: "include",
+        })
+    
+        if(res.status !== 200 && req.nextUrl.pathname.startsWith("/dashboard")) {
+            return NextResponse.redirect(new URL("/login", req.url));
+        }
+    
+        return NextResponse.next()
+    } catch {
         return NextResponse.redirect(new URL("/login", req.url));
     }
 
-    return NextResponse.next()
 }
 
 export const config = {
