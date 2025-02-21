204dd3d362430a85da510b1bc979fc35fe643269
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-21 14:02:51 -0300

    Expansão do banco de dados temporário para persistência de dados

--- backend/src/config/fakeDB.ts
+++ backend/src/config/fakeDB.ts
@@ -1,13 +1,31 @@
 // TEMPORARY: Dummy database. Replace it with a real one in the future.
+import fs from "fs";
+import path from "path";
 import { hashPassword } from "../utils/hash"
 
-export const fakeDB = {
-    users: [] as { email: string, password: string }[],
-};
+const dbPath = path.join(process.cwd(), "src", "db.json");
 
-async function initializeDB() {
-    const hashedPassword = await hashPassword("password123");
-    fakeDB.users.push({ email: "test@email.com", password: hashedPassword })
+function loadUsers(): { email: string; password: string }[] {
+    try {
+        const data = fs.readFileSync(dbPath, "utf-8");
+        const db = JSON.parse(data);
+        return db.users;
+    } catch (error) {
+        return [];
+    }
 }
 
-initializeDB().then(() => console.log("fakeDB initialized"));
+function saveUsers(users: { email: string; password: string }[]) {
+    fs.writeFileSync(dbPath, JSON.stringify(users, null, 2));
+}
+
+export const fakeDB = {
+    users: loadUsers(),
+    addUsers: async (email: string, password: string) => {
+        const hashedPassword = await hashPassword(password);
+        const newUser = { email, password: hashedPassword};
+        fakeDB.users.push(newUser);
+        saveUsers(fakeDB.users);
+    },
+    findUser: (email: string) => fakeDB.users.find((u) => u.email === email),
+};
