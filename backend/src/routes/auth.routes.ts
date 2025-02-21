782885f233e46f9677ba6a409befb078c1545434
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-21 14:10:11 -0300

    Padronização da estrutura do código

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -13,7 +13,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.post("/register", register);
 router.post("/refresh", refreshToken);
-router.get("/me", authMiddleware, requestNewToken)
-router.get("/protected", authMiddleware, protectedRoute)
+router.get("/me", authMiddleware, requestNewToken);
+router.get("/protected", authMiddleware, protectedRoute);
 
 export default router;
