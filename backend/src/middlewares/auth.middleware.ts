61da46ad0c544fb25204a8c2c224d3ed3f350e64
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-21 14:09:16 -0300

    Adição de verificação isTokenInvalid, para confirmar se o token não está listado como inválido na aplicação

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,21 +1,22 @@
 import { Response, NextFunction } from "express";
 import { verifyToken } from "../utils/jwt";
 import { AuthRequest } from "../types/authRequest";
+import { isTokenInvalid } from "../config/tokenStore";
 
 export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction): void => {
     const token = req.cookies?.token;
 
-    if (!token) {
+    if (!token || isTokenInvalid(token)) {
         res.status(401).json({ message: "Access denied. No token provided." });
         return;
-    } else {
-        try {
-            // FUTURE: Implement user search in the database after validating the token.
-            req.user = verifyToken(token);
-            next();
-        } catch (error) {
-            res.status(401).json({ message: "Invalid token." });
-        }
+    }
+    
+    try {
+        // FUTURE: Implement user search in the database after validating the token.
+        req.user = verifyToken(token);
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Invalid token." });
     }
 }
 
