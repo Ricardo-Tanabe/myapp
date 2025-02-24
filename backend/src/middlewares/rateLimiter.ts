8a256417676b9313f130c70d054c0205f7c19bba
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-24 10:42:57 -0300

    Adição de tentativas de login
    O usuário tem algumas tentativas antes do bloqueio do login por um período de tempo

---
+++ backend/src/middlewares/rateLimiter.ts
@@ -0,0 +1,8 @@
+import rateLimit from "express-rate-limit";
+
+export const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 3,
+    message: { message: "Too many login attempts, please try again later" }
+});
+
