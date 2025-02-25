708ad3fb78f3ec177bee7eb016fbabe26a35c88f
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-25 10:47:01 -0300

    Atualização de segurança no app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from 'cors';
 import authRoutes from "./routes/auth.routes";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import csrf from "./middlewares/csrf";
 
 const app = express();
 
@@ -11,9 +13,13 @@ app.use(cors({ // Allows the frontend to access the backend
     credentials: true,
 }));
 
+app.use(helmet()); // Help secure Express apps by setting HTTP response headers.
+
 app.use(cookieParser()); // Allows the backend to read cookies in req.cookies
 
 app.use(express.json()); // Allows receiving JSON in req.body
+
+app.use(csrf);
 
 app.use("/api/auth", authRoutes); // Authorization routes
 
