a52d08b3c64440b3e41f30afc1f66e3dbb0e9be6
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-02-25 10:26:06 -0300

    Limita a quantidade de login seguidos por usuário

--- backend/src/middlewares/rateLimiter.ts
+++ backend/src/middlewares/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const loginLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 3,
+    max: 5,
     message: { message: "Too many login attempts, please try again later" }
-});
+})
 
