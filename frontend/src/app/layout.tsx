1faa3656fa780549e00708b8befe32ab1b31cfdb
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-03-04 20:41:38 -0300

    Desanexando Header e Footer da Homepage
    Estilização focada no conteúdo main da homepage

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ClientWrapper from "@/components/ClientWrapper";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -36,6 +37,7 @@ export default function RootLayout({
         <ClientWrapper>
           <Header />
           {children}
+          <Footer />
         </ClientWrapper>
       </body>
     </html>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,18 +3,9 @@
 import Link from "next/link";
 
 interface ButtonLinkProp {
-  id: number,
   name:string,
   linkName: string,
   css: string
-}
-
-function Footer() {
-  return(
-    <>
-      <footer className={`border-test bg-white h-24`}></footer>
-    </>
-  )
 }
 
 function ButtonLink({name, linkName, css}: ButtonLinkProp) {
@@ -27,36 +18,20 @@ function ButtonLink({name, linkName, css}: ButtonLinkProp) {
   );
 }
 
-function ButtonContainer({buttons, cssAuth}: {buttons: ButtonLinkProp[], cssAuth:string}) {
-  return (
-    <div className={cssAuth}>
-      {buttons.map(({ id, name, linkName, css }) => (
-        <ButtonLink key={id} id={id} name={name} linkName={linkName} css={css} />
-      ))}
-    </div>
-  );
-}
-
 export default function Home() {
-  const cssAuth = "flex justify-center gap-4"
-
-  const buttonsAuth: ButtonLinkProp[] = [
-    { id: 1, name: "Login", linkName: "/login",
-      css: "bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded transition" },
-    { id: 2, name: "Create Account", linkName: "/register",
-      css: "bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded transition" }
-  ];
 
   return (
     <>
-      <main className={`border-test flex-norm-col min-h-screen bg-gray-100 p-6`}>
+      <main className={`border-test flex-norm-col min-h-screen bg-blue-500 p-6`}>
         <h1 className="text-4xl font-bold text-gray-800 mb-4">Full Stack Development Space</h1>
         <p className="text-gray-600 mb-6">
           Explore the platform. Log in to access more features.
         </p>
-        <ButtonContainer buttons={buttonsAuth} cssAuth={cssAuth}/>
+        <div className="flex justify-center gap-4">
+          <ButtonLink name={"Login"} linkName={"/login"} css={"main-button-login"} />
+          <ButtonLink name={"Create Account"} linkName={"/register"} css={"main-button-register"} />
+        </div>
       </main>
-      <Footer />
     </>
   );
 }
