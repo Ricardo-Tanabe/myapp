d6c16f4216f815c07ad06e0e8232dd88d9f53496
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-03-19 14:18:39 -0300

    Função que visa distribuir os elementos nas colunas conforme seu tamanho

--- frontend/src/functions/arrangeElements.ts
+++ frontend/src/functions/arrangeElements.ts
@@ -1,4 +1,16 @@
-import { Subject } from "@/components/header/HeaderTypes";
+import { Subject } from "@/components/header/components/HeaderTypes";
+
+function extractMainElements(container: HTMLDivElement) {
+  const containerChild = Array.from(container.children) as HTMLDivElement[];
+  const items: HTMLDivElement[] = [];
+
+  containerChild.forEach((divChild) => {
+    const elements = Array.from(divChild.children) as HTMLDivElement[];
+    items.push(...elements);
+  })
+
+  return items;
+}
 
 function verifyContainerSpace(
   container: HTMLDivElement,
@@ -20,16 +32,54 @@ function verifyContainerSpace(
   return count;
 }
 
-function mainElements(container: HTMLDivElement) {
-  const containerChild = Array.from(container.children) as HTMLDivElement[];
-  const items: HTMLDivElement[] = [];
+function distributeItemsByHeight(
+  analyzeItems: HTMLDivElement[],
+  standardColumn: HTMLDivElement[][],
+  heightSum: number[]) {
+    
+    for (const item of analyzeItems) {
+      let index = 0;
+      for (let i = 1; i < heightSum.length; i ++) {
+          index = (heightSum[index] <= heightSum[i]) ? index : i;
+      }
+      standardColumn[index].push(item);
+      heightSum[index] += item.offsetHeight;
+    }
 
-  containerChild.forEach((divChild) => {
-    const elements = Array.from(divChild.children) as HTMLDivElement[];
-    items.push(...elements);
-  })
+    return { standardColumn, heightSum };
+}
 
-  return items;
+function distributeColumns(
+  items: HTMLDivElement[],
+  columnNumber: number
+) {
+  const sortedItems = [...items].sort((a, b) => b.offsetHeight - a.offsetHeight);
+
+  let standardColumn: HTMLDivElement[][] = new Array(columnNumber)
+  .fill(null).map(() => []);
+  let heightSum: number[] = new Array(columnNumber).fill(0);
+  
+  const result = distributeItemsByHeight(sortedItems, standardColumn, heightSum);
+  standardColumn = result.standardColumn;
+
+   return standardColumn;
+}
+
+function organizeColumns(subjects: Subject[], standardColumn: HTMLDivElement[][]) {
+  const subjectNames = new Map(subjects.map((item, idx) => [item.subject, idx]));
+
+  const sortedColumns = standardColumn.map((col) =>
+    col.sort((a, b) => {
+      const subjectA = a.querySelector("h3")?.textContent || "";
+      const subjectB = b.querySelector("h3")?.textContent || "";
+      return (subjectNames.get(subjectA) ?? Infinity) - (subjectNames.get(subjectB) ?? Infinity);
+    })).sort((a, b) => {
+    const subjectA = a[0].querySelector("h3")?.textContent || "";
+    const subjectB = b[0].querySelector("h3")?.textContent || "";
+    return (subjectNames.get(subjectA) ?? Infinity) - (subjectNames.get(subjectB) ?? Infinity);
+  });
+
+  return sortedColumns;
 }
 
 export function arrangeElements(
@@ -39,33 +89,11 @@ export function arrangeElements(
   const container = containerRef.current;
   if(!container) return;
 
-  const items = mainElements(container)
-  
-  const columnNumber = verifyContainerSpace(container, items);
-
-  const subjectNames = new Map(subjects.map((item, idx) => [item.subject, idx]));
-
-  const sortedItems = [...items].sort((a, b) => b.offsetHeight - a.offsetHeight);
-
-  const standardColumn: HTMLDivElement[][] = new Array(columnNumber)
-  .fill(null).map(() => []);
-  const heightSum = new Array(columnNumber).fill(0);
-  
-  for (const item of sortedItems) {
-    let index = 0;
-    for (let i = 1; i < heightSum.length; i ++) {
-        index = (heightSum[index] <= heightSum[i]) ? index : i;
-    }
-    standardColumn[index].push(item);
-    heightSum[index] += item.offsetHeight;
-  }
-
-  const columnsResult = standardColumn.map((col) =>
-    col.sort((a, b) => {
-      const subjectA = a.querySelector("h3")?.textContent || "";
-      const subjectB = b.querySelector("h3")?.textContent || "";
-      return (subjectNames.get(subjectA) ?? Infinity) - (subjectNames.get(subjectB) ?? Infinity);
-    }));
+  const items = extractMainElements(container);
+  const temp = verifyContainerSpace(container, items)
+  const columnNumber = temp > 2 ? 3 : temp;
+  const standardColumn = distributeColumns(items, columnNumber);
+  const columnsResult = organizeColumns(subjects, standardColumn);
 
   return columnsResult.map((col) => col.map((el) => ({ id: el.dataset.id, content: el.innerHTML })));
 }
