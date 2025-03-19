453e11cdc8d18b01e0ce5f1de4ebdc30b2eee7b9
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-03-19 14:19:48 -0300

    Atualização dos elementos de importação

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ClientWrapper from "@/components/ClientWrapper";
 import Header from "@/components/header/Header";
-import Footer from "@/components/Footer";
+import Footer from "@/components/footer/Footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -32,7 +32,8 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable}
+        antialiased flex flex-col items-center`}
       >
         <ClientWrapper>
           <Header />
