0f6128f4f3e7397e28351d0b1f72bf6001aeefa6
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-03-26 15:54:51 -0300

    Correção de loadUsers retornando os dados do db provisório corretos

--- backend/src/config/fakeDB.ts
+++ backend/src/config/fakeDB.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { hashPassword } from "../utils/hash"
 
 interface User {
+    id: number,
     email: string,
     password: string,
     role: "user" | "admin"
@@ -13,9 +14,13 @@ const dbPath = path.join(process.cwd(), "src", "db.json");
 
 function loadUsers(): User[] {
     try {
+        if(!fs.existsSync(dbPath)) {
+            fs.writeFileSync(dbPath, JSON.stringify([], null, 2));
+            return [];
+        }
         const data = fs.readFileSync(dbPath, "utf-8");
         const db = JSON.parse(data);
-        return db.users;
+        return db;
     } catch (error) {
         return [];
     }
@@ -27,11 +32,11 @@ function saveUsers(users: User[]) {
 
 export const fakeDB = {
     users: loadUsers(),
-    addUsers: async ({email, password, role = "user"}: User) => {
+    addUsers: async ({id, email, password, role = "user"}: User) => {
         const hashedPassword = await hashPassword(password);
-        const newUser = { email, password: hashedPassword, role};
+        const newUser = { id, email, password: hashedPassword, role};
         fakeDB.users.push(newUser);
         saveUsers(fakeDB.users);
     },
-    findUser: (email: string) => fakeDB.users.find((u) => u.email === email),
+    findUser: (email: string) => fakeDB.users.find((u) => u.email === email)
 };
