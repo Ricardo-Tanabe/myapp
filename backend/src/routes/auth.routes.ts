04bcd2cabd2bde14161fb3eac9d06b078c189834
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-03-28 14:33:20 -0300

    Verificação de erros assincronos nos callbacks de router

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,25 +1,21 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware";
-import { loginLimiter } from "../middlewares/rateLimiter";
-import { roleMiddleware } from "../middlewares/role.middleware";
-import { login,
-         logout,
-         register,
-         refreshToken,
-         sendCsrf,
+import { loginLimiter } from "../middlewares/rateLimiter.middleware";
+import { roleMiddleware } from "../middlewares/role.middleware"; // Blocks users without permission
+import { login, logout, register,
+         refreshToken, sendCsrf,
          requestNewToken,
          protectedRoute } from "../controllers/auth.controller";
+import asyncHandler from "../middlewares/asyncHandler.middleware"; //Middleware to automatically catch async errors
 
 const router = Router();
 
-router.post("/login", loginLimiter, login);
-router.post("/logout", logout);
-router.post("/register", register);
-router.post("/refresh", refreshToken);
-router.get("/csrf-token", sendCsrf);
-// roleMiddleware("admin") is just a test to see if it blocks users
-// without permission.
-router.get("/me", authMiddleware, requestNewToken); //, roleMiddleware("admin")
-router.get("/protected", authMiddleware, protectedRoute);
+router.post("/login", loginLimiter, asyncHandler(login));
+router.post("/logout", asyncHandler(logout));
+router.post("/register", asyncHandler(register));
+router.post("/refresh", asyncHandler(refreshToken));
+router.get("/csrf-token", asyncHandler(sendCsrf));
+router.get("/me", authMiddleware, asyncHandler(requestNewToken));
+router.get("/protected", authMiddleware, asyncHandler(protectedRoute));
 
 export default router;
