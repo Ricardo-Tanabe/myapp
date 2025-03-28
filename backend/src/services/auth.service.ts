67bd4ebe34ea7514b5937085cfa7eaeef36e3e20
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-03-28 14:34:23 -0300

    Implementação de blocos try/catch para captura de erros

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -6,29 +6,39 @@ class AuthService {
     static async register(email: string, password: string) {
         // TEMPORARY: Users are being stored in memory.
         // When a database is added, remove this logic.
-        if(fakeDB.findUser(email)) {
-            console.log("User already exists");
-            throw new Error("User already exists");
+        try {
+            if(fakeDB.findUser(email)) {
+                console.log("User already exists");
+                throw new Error("User already exists");
+            }
+            const newUser = await fakeDB.addUser({ email: email, password: password, role: "user"});
+            return { message: "User registered successfully", userId: newUser.id}
+        } catch (error) {
+            console.error("Registration error:")
+            throw new Error("Error registering user")
         }
-        const userID = fakeDB.users.length + 1;
-        await fakeDB.addUsers({ id: userID ,email: email, password: password, role: "user"});
     }
 
     static async login(email: string, password: string) {
-        const user = fakeDB.findUser(email);
-        if(!user || !(await comparePassword(password, user.password))) {
-            console.log("Credenciais inválidas")
-            throw new Error("Invalid credentials");
+        try {
+            const user = fakeDB.findUser(email);
+            if(!user || !(await comparePassword(password, user.password))) {
+                throw new Error("Invalid credentials");
+            }
+    
+            const payload = { userId: email, role: user.role}
+    
+            return {
+                // Add unique identifier after implementing the database
+                // by replacing email
+                token: generateToken(payload),
+                refreshToken: generateRefreshToken(payload),
+                message: "Login successful"
+            };
+        } catch (error) {
+            console.error("Login error: ", error);
+            throw new Error("Error logging in");
         }
-
-        const payload = { userId: email, role: user.role}
-
-        return {
-            // Add unique identifier after implementing the database
-            // by replacing email
-            token: generateToken(payload),
-            refreshToken: generateRefreshToken(payload),
-        };
     }
 }
 
