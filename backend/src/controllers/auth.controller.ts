1181d8b267bb9251c810e8f228ded86e538e648a
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-03-28 14:31:47 -0300

    Verificação de erros melhorado.

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,19 +1,11 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import AuthService from "../services/auth.service";
 import { verifyRefreshToken, generateToken, generateRefreshToken } from "../utils/jwt";
 import { body, validationResult } from "express-validator";
 import { AuthRequest } from "../types/authRequest";
 import { addInvalidToken, isTokenInvalid } from "../config/tokenStore";
 
-const handleErrorResponse = (res: Response, error: unknown) => {
-    if(error instanceof Error)  {
-        res.status(400).json({ message: error.message });
-        return;
-    }
-    res.status(400).json({ message: "An unexpected error occurred" });
-}
-
-export const register = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     await body("email").isEmail().normalizeEmail().run(req);
     await body("password").trim().isLength({ min: 6 }).run(req);
 
@@ -24,14 +16,14 @@ export const register = async (req: Request, res: Response) => {
     }
 
     try {
-        await AuthService.register(req.body.email, req.body.password)
-        res.status(201).json({ message: "User registered successfully" });
+        const { message, userId } = await AuthService.register(req.body.email, req.body.password)
+        res.status(201).json({ message: message });
     } catch(error) {
-        handleErrorResponse(res, error);
+        next(error);
     }
 }
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     await body("email").isEmail().normalizeEmail().run(req);
     await body("password").trim().isLength({ min: 6 }).run(req);
 
@@ -42,7 +34,7 @@ export const login = async (req: Request, res: Response) => {
     }
     
     try {
-        const { token, refreshToken} = await AuthService.login(req.body.email, req.body.password);
+        const { token, refreshToken, message} = await AuthService.login(req.body.email, req.body.password);
 
         res.cookie("token", token, {
             httpOnly: true,
@@ -59,13 +51,13 @@ export const login = async (req: Request, res: Response) => {
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
-        res.json({ message: "Login successful" });
+        res.json({ message: message });
     } catch(error) {
-        handleErrorResponse(res, error);
+        next(error);
     }
 }
 
-export const refreshToken = (req: Request, res: Response) => {
+export const refreshToken = async (req: Request, res: Response): Promise<void> => {
     const refreshToken = req.cookies?.refreshToken;
 
     if(!refreshToken || isTokenInvalid(refreshToken)) {
@@ -95,7 +87,7 @@ export const refreshToken = (req: Request, res: Response) => {
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
-        res.status(200).json({ message: "Atend Request" })
+        res.status(200).json({ message: "Token refreshed" })
 
         // When the token is added to setImmediate, before the above cookie
         // is sent, it is already invalidated.
@@ -105,7 +97,7 @@ export const refreshToken = (req: Request, res: Response) => {
     }
 }
 
-export const logout = (req: Request, res: Response) => {
+export const logout = async (req: Request, res: Response): Promise<void> => {
     const refreshToken = req.cookies?.refreshToken;
 
     if(refreshToken) {
@@ -130,14 +122,14 @@ export const logout = (req: Request, res: Response) => {
     res.status(200).json({ message: "Logout completed successfully" });
 }
 
-export const sendCsrf = (req: Request, res: Response) => {
+export const sendCsrf = async (req: Request, res: Response): Promise<void> => {
     res.json({ csrfToken: req.csrfToken() });
 }
 
-export const requestNewToken = (req: AuthRequest, res: Response) => {
-    res.json({ user:req.user });
+export const requestNewToken = async (req: AuthRequest, res: Response): Promise<void> => {
+    res.status(200).json({ user:req.user });
 }
 
-export const protectedRoute = (req: AuthRequest, res: Response) => {
+export const protectedRoute = async (req: AuthRequest, res: Response): Promise<void> => {
     res.json({ message: "You have access to this protected route!", user:req.user });
 }
