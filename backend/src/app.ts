96d0d3958972b72d82aa3fd446d212b49e36990a
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-03-28 14:44:15 -0300

    Adição de verificação de erros na aplicação

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 import cors from 'cors';
 import authRoutes from "./routes/auth.routes";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
-import csrf from "./middlewares/csrf";
+import csrf from "./middlewares/csrf.middleware";
+import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 
@@ -13,14 +14,27 @@ app.use(cors({ // Allows the frontend to access the backend
     credentials: true,
 }));
 
-app.use(helmet()); // Help secure Express apps by setting HTTP response headers.
+app.use(helmet({
+    contentSecurityPolicy: false, //Disable CSP to avoid unwanted blocking (adjust as needed)
+})); // Help secure Express apps by setting HTTP response headers.
 
 app.use(cookieParser()); // Allows the backend to read cookies in req.cookies
 
-app.use(express.json()); // Allows receiving JSON in req.body
+// Invalid JSON handling
+app.use(express.json({ limit: "10kb" })); // Allows receiving JSON in req.body. The parameter set a limit to avoid large payloads
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof SyntaxError && "body" in err) {
+        res.status(400).json({ error: "Invalid JSON format" });
+        return;
+    }
+    next();
+})
 
 app.use(csrf);
 
 app.use("/api/auth", authRoutes); // Authorization routes
+
+app.use(errorMiddleware);
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,25 @@ import app from "./app";
 const PORT = Number(process.env.PORT) || 3001;
 const HOST = "localhost"
 
-app.listen(PORT, HOST, () => {
-    console.log(`Server running on http://${HOST}:${PORT}`);
+const startServer = () => {
+    try {
+        app.listen(PORT, HOST, () => {
+            console.log(`Server running on http://${HOST}:${PORT}`);
+        })
+    } catch (error) {
+        console.error("Error starting server: ", error);
+        process.exit(1);
+    }
+};
+
+process.on("uncaughtException", (error) => {
+    console.error("Uncaught Exception: ", error);
+    process.exit(1);
 });
+
+process.on("unhandledRejection", (reason, promise) => {
+    console.error("Unhandled Rejection at: ", promise, " reason: ", reason);
+    process.exit(1);
+})
+
+startServer();
